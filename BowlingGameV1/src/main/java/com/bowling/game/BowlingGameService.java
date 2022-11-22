f992c0ba77a4c2159d71423d693a9b40c65b8179
Ayushi <HP@DESKTOP-KQA8LBN>
2022-11-22 18:26:35 +0530

    Feat : Test case when a gutter ball hit zero pins nothing will add in a score therefore expecting output as 0

--- BowlingGameV1/src/main/java/com/bowling/game/BowlingGameService.java
+++ BowlingGameV1/src/main/java/com/bowling/game/BowlingGameService.java
@@ -9,4 +9,12 @@ public class BowlingGameService {
 	public void rollingABowlInBowlingGameTest(int pinDows) {
 		System.out.println("Calling a method to roll a bowl");
 	}
+	
+	public void rollingABowl_InBowlingGameTest(int pinDows) {
+		System.out.println("Rolling a Gutter ball...");
+	}
+
+	public int getScoreAfterBowlHits() {
+		return 0;
+	}
 }
--- BowlingGameV1/src/test/java/com/bowlingtest/game/BowlingGameServiceTests.java
+++ BowlingGameV1/src/test/java/com/bowlingtest/game/BowlingGameServiceTests.java
@@ -1,5 +1,8 @@
 package com.bowlingtest.game;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,5 +26,14 @@ public class BowlingGameServiceTests {
 	public void getARollBowlInBowlingGame() {
 		BowlingGameService bowlingGameService = new BowlingGameService();
 		bowlingGameService.rollingABowlInBowlingGameTest(0);  //to simply roll a bowl
+	}
+
+	@Test
+	public void getARollerBowlScoringAGutterBowlTests() {
+		for (int i = 0; i < 20; i++) {
+			bowlingGameService.rollingABowl_InBowlingGameTest(0);
+
+		}
+		assertThat(bowlingGameService.getScoreAfterBowlHits(), is(0));
 	}
 }
